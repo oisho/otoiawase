c393080e13834e59b0cdfd5a128d6caae4b75922
oisho <vagrant@localhost.localdomain>
2016-02-11 12:20:54 +0000

    ルーティングファイル
    ルーティングっぽい何かを作ってみました。

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,39 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+// $app->get('/[{name}]', function ($request, $response, $args) {
+//     // Sample log message
+//     $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+//     // Render index view
+//     return $this->renderer->render($response, 'index.phtml', $args);
+// });
+
+$app->get('/contact', function ($request, $response, $args) {
+    //まだ未作成
+    $action = new Slim\App\Contact
+    $action -> run();
+
+    //まだ未作成
+    return $this->renderer->render($response, 'contact.html', $args);
+});
+
+
+$app->get('/confirm/{}', function ($request, $response, $args) {
+    //まだ未作成
+    $action = new Slim\App\Confirm
+    $action -> run();
+
+    //まだ未作成
+    return $this->renderer->render($response, 'confirm.html', $args);
+});
+
+
+$app->get('/completion', function ($request, $response, $args) {
+    //まだ未作成
+    $action = new Slim\App\Completion
+    $action -> run();
+
+    //まだ未作成
+    return $this->renderer->render($response, 'completion.html', $args);
 });
