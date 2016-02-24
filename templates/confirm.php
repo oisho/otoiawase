74583060d327281bb2e3a92705237e4eb907ef77
YSO <cpo@AvecLab140704a-no-MacBook-Air.local>
2016-02-24 20:06:07 +0900

    HTML/CSSを書きました#10中ですが、ひとまず書きました。

--- templates/confirm.php
+++ templates/confirm.php
@@ -5,33 +5,29 @@
   <title>確認画面</title>
   <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-  <h>本当にこれで大丈夫？？</h>
+
+  <h1>CHECK OUT</h1>
+  <p>内容に誤りがないか、マジでご確認ください！</p>
   <div id="container">
     <p>
-      お名前
+      <label>Name：</label><?php echo htmlspecialchars($_POST['name']); ?>
     </p>
     <p>
-      <?php echo htmlspecialchars($_POST['name']); ?>
+      <label>E-mail：</label><?php echo htmlspecialchars($_POST['email']); ?>
     </p>
-    <p>
-      email
-    </p>
-    <p>
-      <?php echo htmlspecialchars($_POST['email']); ?>
-    </p>
-    <p>
-      本文
-    </p>
-    <p>
+    <fieldset>
+    <legend>本文</legend> 
       <?php echo htmlspecialchars($_POST['opinion']); ?>
-    </p>
+    </fieldset>
+    <br>
     <div style="display:inline-flex">
     <form action="contact" method="post" id="confirm">
       <input type="hidden" name="name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
       <input type="hidden" name="email" value="<?php echo htmlspecialchars($_POST['email']); ?>">
       <input type="hidden" name="opinion" value="<?php echo htmlspecialchars($_POST['opinion']); ?>">
-      <input type="submit" name="submit" value="戻る">
+      <input type="submit" name="submit" value="戻る" id="btn">
     </form>
     　
     <form action="completion" method="post" id="confirm">
@@ -39,9 +35,10 @@
       <input type="hidden" name="email" value="<?php echo htmlspecialchars($_POST['email']); ?>">
       <input type="hidden" name="opinion" value="<?php echo htmlspecialchars($_POST['opinion']); ?>">
       <input type="hidden" name="time" value="<?php echo date("Y-m-d H:i:s"); ?>">
-      <input type="submit" name="submit" value="投稿する">
+      <input type="submit" name="submit" value="投稿する" id="btn">
     </form>
     </div>
   </div>
+  </section>
 </body>
 </html>
--- templates/contact.php
+++ templates/contact.php
@@ -4,30 +4,58 @@
   <meta charset="utf-8">
   <title>お問い合わせフォーム</title>
   <link rel="stylesheet" href="styles.css">
+ <!--  <script type="text/javascript"> 
+<!-- 
+function check(){
+  var flag = 0;
+  // 設定開始（必須にする項目を設定してください）
+  if(document.form1.field1.value == ""){ // 「お名前」の入力をチェック
+    flag = 1;
+  }
+  else if(document.form1.field2.value == ""){ // 「パスワード」の入力をチェック
+    flag = 1;
+  }
+  else if(document.form1.field3.value == ""){ // 「コメント」の入力をチェック
+    flag = 1;
+  }
+  // 設定終了
+
+  if(flag){
+    window.alert('必須項目に未入力があります'); // 入力漏れがあれば警告ダイアログを表示
+    return false; // 送信を中止
+  }
+  else{
+    return true; // 送信を実行
+  }
+}
+</script> -->
 </head>
 <body>
-  ご意見、ご苦情、愛の告白、ええ意見などございましたらこちらから
+<section class="box fourcorners">
+  <h1>CONTACT US</h1>
+  <p>ご意見、ご苦情、愛の告白などございましたらこちらから</p>
   <div id="container">
     <form action="confirm" method="post" id="contact">
       <p>
-        <input type="text" name="name" placeholder="name" value="<?php echo htmlspecialchars($_POST['name']); ?>" required>
+        <input type="text" name="name" placeholder="Name" value="<?php echo htmlspecialchars($_POST['name']); ?>" required id="namen">
       </p>
       <p>
       <!-- お名前エラーメッセージ -->
       </p>
       <p>
-        <input type="email" name="email" placeholder="email" value="<?php echo htmlspecialchars($_POST['email']); ?>" required>
+        <input type="email" name="email" placeholder="E-Mail" value="<?php echo htmlspecialchars($_POST['email']); ?>" required id="mail">
       </p>
       <!-- emailエラーメッセージ -->
       <p>
-        <textarea name="opinion" rows="4" cols="40" placeholder="こちらに本文を記入してください" required><?php echo htmlspecialchars($_POST['opinion']); ?></textarea>
+        <textarea name="opinion" rows="4" cols="40" placeholder="こちらに本文を記入してください" required id="opinion"><?php echo htmlspecialchars($_POST['opinion']); ?></textarea>
       </p>
       <p>
-      <input type="submit" name="submit" value="次へ">
+      <input type="submit" name="submit" value="確認画面へ" id="btn">
       </p>
-      <!-- <div class="btn" onclick="document.getElementById('contact').submit();">次へ</div> -->
-      <input type="hidden" name="token" value="<?php htmlspecialchars($_SESSION['token']); ?>">
+       <!-- <div class="btn" onclick="document.getElementById('contact').submit();">確認画面</div> -->
+       <input type="hidden" name="token" value="<?php htmlspecialchars($_SESSION['token']); ?>">
     </form>
   </div>
+  <section>
 </body>
 </html>
